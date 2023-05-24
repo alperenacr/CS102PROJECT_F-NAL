90b9474e5a9c6df0fa055338e24abb7a8b14b0ae
alperenacr <alprnacar@hotmail.com>
2023-05-24 18:57:24 +0300

    pick up component halledildi animasyon işine girmedim

--- SpaceGame/src/main/spacegame/component/PickUpComponent.java
+++ SpaceGame/src/main/spacegame/component/PickUpComponent.java
@@ -1,5 +1,25 @@
 package main.spacegame.component;
 
-public class PickUpComponent {
+
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.component.Component;
+import main.spacegame.SpaceGameApp;
+
+public class PickUpComponent extends Component {
+    private Runnable onPickup;
+
+    public void PickupComponent(Runnable onPickup) {
+        this.onPickup = onPickup;
+
+    }
+
+    public void pickUp() {
+
+        onPickup.run();
+    }
+
     
+
+
 }
