d4bb0d1f2fcbbd94850baf6083692e5cd0db8693
Yigit <yigit.ozhan@ug.bilkent.edu.tr>
2023-05-25 15:23:53 +0300

    DashComponent bitti

--- SpaceGame/src/main/spacegame/component/DashComponent.java
+++ SpaceGame/src/main/spacegame/component/DashComponent.java
@@ -1,5 +1,30 @@
 package main.spacegame.component;
 
+import com.almasb.fxgl.entity.Entity;
+
 public class DashComponent {
-    
+
+    Entity player;
+    int dashAmount;
+
+    DashComponent(Entity player, int dashAmount){
+        this.player = player;
+        this.dashAmount = dashAmount;
+    }
+
+    public void dashUp() {
+        player.translateY(dashAmount);
+    }
+    public void dashDown() {
+        player.translateY(-dashAmount);
+    }
+    public void dashRight() {
+        player.translateX(dashAmount);
+    }
+    public void dashLeft() {
+        player.translateX(-dashAmount);
+    }
+
+
+
 }
