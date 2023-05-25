ba51d19fe3564e2115ebd9c7012ddded8efea7b1
Yigit <yigit.ozhan@ug.bilkent.edu.tr>
2023-05-25 22:42:16 +0300

    WingedAlienComponent bitti

--- SpaceGame/src/main/spacegame/component/enemy/WingedAlienComponent.java
+++ SpaceGame/src/main/spacegame/component/enemy/WingedAlienComponent.java
@@ -1,5 +1,92 @@
 package main.spacegame.component.enemy;
 
-public class WingedAlienComponent {
-    
+
+import com.almasb.fxgl.core.math.Vec2;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.component.Component;
+import javafx.geometry.Point2D;
+
+public class WingedAlienComponent extends Component {
+    private int moveSpeed;
+    private Vec2 velocity = new Vec2();
+    private Entity wingedAlien;
+    Vec2 directionVector;
+    private Entity player;
+
+    WingedAlienComponent(int moveSpeed, Entity player) {
+        this.moveSpeed = moveSpeed;
+        this.player = player;
+    }
+
+    @Override
+    public void onAdded() {
+        wingedAlien = entity;
+    }
+
+    public void onUpdate(double tpf) {
+        move(moveSpeed);
+    }
+
+    private void move(double tpf) {
+        
+        directionVector = velocitySetter();
+
+        velocity.addLocal(directionVector).mulLocal((float)tpf);
+
+        wingedAlien.translate(new Point2D(velocity.x, velocity.y));;
+    }
+
+
+    private Vec2 velocitySetter() {
+        if (wingedAlien.getY() > player.getY()) {
+            if (wingedAlien.getX() == player.getX()) {
+                return new Vec2(0,1);
+            }
+            else if (wingedAlien.getX() < player.getX()){
+                return new Vec2(1,0);
+            }
+            else {
+                return new Vec2(-1,0);
+            }
+        }
+
+        else if (wingedAlien.getY() < player.getY()) {
+            if (wingedAlien.getX() == player.getX()) {
+                return new Vec2(0,-1);
+            }
+            else if (wingedAlien.getX() < player.getX()){
+                return new Vec2(1,0);
+            }
+            else {
+                return new Vec2(-1,0);
+            }
+        }
+
+        else if (wingedAlien.getX() > player.getX()) {
+            if (wingedAlien.getY() == player.getY()) {
+                return new Vec2(-1, 0);
+            }
+            else if (wingedAlien.getY() < player.getY()){
+                return new Vec2(0, -1);
+            }
+            else {
+                return new Vec2(0, 1);
+            }
+        }
+
+        else if (wingedAlien.getX() < player.getX()) {
+            if (wingedAlien.getY() == player.getY()) {
+                return new Vec2(1, 0);
+            }
+            else if (wingedAlien.getY() < player.getY()){
+                return new Vec2(0, -1);
+            }
+            else {
+                return new Vec2(0, 1);
+            }
+        }
+        
+        return new Vec2(0,0);
+    }
+
 }
