0e8948f6e3826d60034b66fd1986130998ab2f32
Yigit <yigit.ozhan@ug.bilkent.edu.tr>
2023-05-27 04:16:17 +0300

    BulletComponent bitti

--- SpaceGame/src/main/spacegame/component/BulletComponent.java
+++ SpaceGame/src/main/spacegame/component/BulletComponent.java
@@ -1,5 +1,43 @@
 package main.spacegame.component;
 
-public class BulletComponent {
+import com.almasb.fxgl.dsl.components.AccumulatedUpdateComponent;
+import com.almasb.fxgl.dsl.components.ExpireCleanComponent;
+import com.almasb.fxgl.dsl.components.ProjectileComponent;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.component.Component;
+import main.spacegame.SpaceGameType;
+import javafx.geometry.Point2D;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+
+public class BulletComponent extends AccumulatedUpdateComponent {
+
+    private Entity grid;
+    private GridComponent gComponent;
+    private Point2D velocity;
     
+
+    public BulletComponent() {
+        super(5);
+    }
+
+    @Override
+    public void onAdded() {
+        velocity = entity.getComponent(ProjectileComponent.class).getVelocity();
+    }
+
+    @Override
+    public void onAccumulatedUpdate(double tpf) {
+        if (grid == null) {
+            grid = getGameWorld().getSingleton(SpaceGameType.GRID);
+            gComponent = grid.getComponent(GridComponent.class);
+        }
+
+        
+        var p = entity.getPosition().add(entity.getTransformComponent().getRotationOrigin());
+
+        gComponent.applyImplosiveForce(velocity.magnitude() / 60 * 10, p, 80 * 60 * tpf / getNumFramesToSkip());
+    }
+
 }
