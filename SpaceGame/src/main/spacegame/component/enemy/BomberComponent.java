548d9e6708b10a9ce95edce7014ec5fa1691ce08
Yiğit Özhan <114230568+Trilla01@users.noreply.github.com>
2023-05-27 03:33:13 +0300

    BomberComponent bitti

--- SpaceGame/src/main/spacegame/component/enemy/BomberComponent.java
+++ SpaceGame/src/main/spacegame/component/enemy/BomberComponent.java
@@ -1,7 +1,19 @@
 package main.spacegame.component.enemy;
 
+import main.spacegame.factory.SpaceGameFactory;
+import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.component.Component;
 
-public class BomberComponent extends Component{
-    
+public class BomberComponent extends AmongUsComponent{
+
+    SpaceGameFactory factory = new SpaceGameFactory();
+
+    BomberComponent(int moveSpeed, Entity player){
+        super(moveSpeed, player);
+    }
+
+    @Override
+    public void onUpdate(double tpf) {
+        factory.spawnBomb();
+    }
 }
