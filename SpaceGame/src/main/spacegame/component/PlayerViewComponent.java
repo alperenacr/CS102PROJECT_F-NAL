696508494eec46776921558389cc14e87ad919eb
mustafa çağlar <114356585+mmustafacaglarr@users.noreply.github.com>
2023-05-29 14:19:46 +0300

    Update PlayerViewComponent.java
    playerViewComponent health attribute added

--- SpaceGame/src/main/spacegame/component/PlayerViewComponent.java
+++ SpaceGame/src/main/spacegame/component/PlayerViewComponent.java
@@ -1,5 +1,19 @@
 package main.spacegame.component;
 
-public class PlayerViewComponent {
+public class PlayerViewComponent extends ChildViewComponent {
     
+    public PlayerViewComponent()      
+    {
+        super(35, 35, false);
+
+        var playerHealth = new Arc(0, 0, 60, 60, -100, 0);
+        
+        playerHealth.setStroke(Color.BLACK.brighter());
+        playerHealth.setStrokeWidth(3.0);
+        playerHealth.setFill(null);
+        playerHealth.lengthProperty().bind(getip("hp").multiply(-360.0).divide(PLAYER_HP));
+        
+        getViewRoot().getChildren().addAll(playerHealth);
+     }
+
 }
